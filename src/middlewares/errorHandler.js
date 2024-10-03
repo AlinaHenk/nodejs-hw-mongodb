cf6db9a2a1979243851efc67af07c801380d1b5b
AlinaHenk <160937471+AlinaHenk@users.noreply.github.com>
2024-10-03 12:38:15 +0300

    completed

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (error, req, res, next) => {
   const { status = 500, message } = error;
   res.status(status).json({
-    status: 500,
+    status,
     message: 'Something went wrong',
     data: message,
   });
