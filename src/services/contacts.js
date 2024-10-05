6679e3c8e76b7ed815bb40ba94fef1504b47455c
AlinaHenk <160937471+AlinaHenk@users.noreply.github.com>
2024-10-05 21:22:50 +0300

    pagination is complete

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,24 @@
 import ContactCollection from '../db/models/contact.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = () => ContactCollection.find();
+export const getAllContacts = async ({ perPage, page }) => {
+  const skip = (page - 1) * perPage;
+  //const contactQuery = ContactCollection.find();
+
+  const contacts = await ContactCollection.find().skip(skip).limit(perPage);
+  //.sort({ [sortBy]: sortOrder })
+  const count = await ContactCollection.find().countDocuments();
+
+  const paginationData = calculatePaginationData({ count, perPage, page });
+
+  return {
+    contacts,
+    page,
+    perPage,
+    totalItems: count,
+    ...paginationData,
+  };
+};
 
 export const getContactById = (id) => ContactCollection.findById(id);
 
