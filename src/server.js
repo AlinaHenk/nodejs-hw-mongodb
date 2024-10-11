90ad673405a110d3ca753bd675791f3e9bb9dba8
AlinaHenk <160937471+AlinaHenk@users.noreply.github.com>
2024-10-11 19:13:56 +0300

    GET contactId swagger is comleted

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import logger from './middlewares/logger.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 export const startServer = () => {
   const app = express();
@@ -22,6 +23,7 @@ export const startServer = () => {
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(notFoundHandler);
   app.use(errorHandler);
